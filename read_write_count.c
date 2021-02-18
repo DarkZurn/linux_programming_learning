48e76c0042c0ba7daf0c6d86c6af010bf98d3395
DarkZurn <33867223+DarkZurn@users.noreply.github.com>
2021-02-18 10:04:16 +1000

    Add files via upload

---
+++ read_write_count.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  char name[100];
+  printf("What is your name? __");
+  gets(name);
+  return printf("Hello %s\n",name);
+}
+
+
+//strace -c -e trace=read,write ./test
